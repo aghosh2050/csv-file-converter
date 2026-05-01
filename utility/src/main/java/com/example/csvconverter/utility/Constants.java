3e3e050e59d7e993da35c2605e2e4c9a2d76e82f
Amit Ghosh <amitghosh.in2050@gmail.com>
2026-05-01 14:53:45 +0530

    Add Constants.java

---
+++ utility/src/main/java/com/example/csvconverter/utility/Constants.java
@@ -0,0 +1,7 @@
+package com.example.csvconverter.utility;
+
+public class Constants {
+
+    public static final String CSV_DELIMITER = ",";
+    public static final String OUTPUT_FILE = "output.csv";
+}
